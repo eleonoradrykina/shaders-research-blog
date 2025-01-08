9db7c56edea2944ce66eb59b448a4d8847c6b693
Eleonora <eleonora.drykina@student.howest.be>
2025-01-08 13:39:46 +0100

    chamged description

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Passion Project Blog";
+export const SITE_DESCRIPTION = "Researching GLSL, WGSL, TSL possibilities and implementations.";
 export const SITE_BASE = "/shaders-research-blog";
