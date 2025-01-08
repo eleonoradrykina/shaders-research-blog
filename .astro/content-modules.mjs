696987ba49e8b1869c02323b24797092f5a95d8c
Eleonora <eleonora.drykina@student.howest.be>
2025-01-08 21:05:00 +0100

    added tsl and headers

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,6 @@
 
 export default new Map([
+['src/content/blog/01-08-first-wgsl-shader.mdx', () => import('astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F01-08-first-wgsl-shader.mdx&astroContentModuleFlag=true')],
 ['src/content/blog/01-07-writing-shaders.mdx', () => import('astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F01-07-writing-shaders.mdx&astroContentModuleFlag=true')],
-['src/content/blog/01-06-start.mdx', () => import('astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F01-06-start.mdx&astroContentModuleFlag=true')],
-['src/content/blog/01-08-first-wgsl-shader.mdx', () => import('astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F01-08-first-wgsl-shader.mdx&astroContentModuleFlag=true')]]);
+['src/content/blog/01-06-start.mdx', () => import('astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F01-06-start.mdx&astroContentModuleFlag=true')]]);
 		
