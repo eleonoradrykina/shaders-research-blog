b8f2e484f74449f27ae48bf32f63641dd24e6afa
Eleonora <eleonora.drykina@student.howest.be>
2025-01-15 16:01:17 +0100

    added disqus

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,11 +1,11 @@
 
 export default new Map([
-['src/content/blog/01-08-first-wgsl-shader.mdx', () => import('astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F01-08-first-wgsl-shader.mdx&astroContentModuleFlag=true')],
 ['src/content/blog/01-06-start.mdx', () => import('astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F01-06-start.mdx&astroContentModuleFlag=true')],
-['src/content/blog/01-07-writing-shaders.mdx', () => import('astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F01-07-writing-shaders.mdx&astroContentModuleFlag=true')],
 ['src/content/blog/01-10-computing-shaders.mdx', () => import('astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F01-10-computing-shaders.mdx&astroContentModuleFlag=true')],
+['src/content/blog/01-08-first-wgsl-shader.mdx', () => import('astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F01-08-first-wgsl-shader.mdx&astroContentModuleFlag=true')],
 ['src/content/blog/01-15-gpgpu.mdx', () => import('astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F01-15-gpgpu.mdx&astroContentModuleFlag=true')],
+['src/content/blog/01-07-writing-shaders.mdx', () => import('astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F01-07-writing-shaders.mdx&astroContentModuleFlag=true')],
+['src/content/blog/01-09-roadmap.mdx', () => import('astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F01-09-roadmap.mdx&astroContentModuleFlag=true')],
 ['src/content/blog/01-14-planning.mdx', () => import('astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F01-14-planning.mdx&astroContentModuleFlag=true')],
-['src/content/blog/01-13-textures.mdx', () => import('astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F01-13-textures.mdx&astroContentModuleFlag=true')],
-['src/content/blog/01-09-roadmap.mdx', () => import('astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F01-09-roadmap.mdx&astroContentModuleFlag=true')]]);
+['src/content/blog/01-13-textures.mdx', () => import('astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F01-13-textures.mdx&astroContentModuleFlag=true')]]);
 		
